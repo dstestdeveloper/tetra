c5e25a7ed244f5154b93c17bff953531a216eaa6
JAEMIN YOON <yjm8440@naver.com>
2023-09-23 16:23:13 +0900

    Feature: 셀프코칭쪽 완성 (pc버전만, 이미지 하나 합쳐달라해야함)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import React from "react";
 import Header from "./components/Header";
 import Intro from "./components/Intro";
+import SelfCoaching from "./components/SelfCoaching";
 
 function App() {
+  //질문1. 셀프코칭쪽 스마일 이미지 합쳐줄 수 있는지
+  //질문2. 셀프코칭쪽 배경색이 #ddd인거같은데 제가 한 것은 좀 더 탁하다.
   return (
     <>
       <Header />
       <Intro />
+      <SelfCoaching />
+      <div style={{ height: "200px" }}></div>
     </>
   );
 }
---
+++ src/components/SelfCoaching.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import styled from "styled-components";
+
+const SelfCoaching: React.FC = () => {
+  return (
+    <SelfCoachingWrapper>
+      <LeftContent>
+        <Start>start!</Start>
+        <Title>셀프코칭 시작하기</Title>
+        <Content>
+          나 자신이 정확히
+          <br /> 누군지 파악해볼까요?
+        </Content>
+        <SelfCoachingImage
+          src={"/images/milestone.png"}
+          alt="milestone"
+          width={541}
+          height={464}
+        />
+      </LeftContent>
+      <RightContent>
+        <SelfCoachingImage
+          src={"/images/phone.png"}
+          alt="phone"
+          width={489}
+          height={869}
+        />
+      </RightContent>
+    </SelfCoachingWrapper>
+  );
+};
+
+const Start = styled.div`
+  font-weight: 700;
+  font-size: 30px;
+  color: #813ff2;
+`;
+
+const Content = styled.div`
+  font-weight: 400;
+  font-size: 36px;
+  color: #1a1f27;
+  margin-top: 27px;
+`;
+
+const Title = styled.div`
+  font-weight: 700;
+  font-size: 50px;
+  color: #1a1f27;
+  margin-top: 13px;
+`;
+
+const RightContent = styled.div``;
+
+const LeftContent = styled.div``;
+
+const SelfCoachingImage = styled.img`
+  object-fit: contain;
+  margin-top: 44px;
+`;
+
+const SelfCoachingWrapper = styled.div`
+  width: 100%;
+  height: 967px;
+  display: inline-flex;
+  justify-content: space-around;
+  align-items: flex-end;
+  background-color: #f9f9f9;
+`;
+
+export default SelfCoaching;
