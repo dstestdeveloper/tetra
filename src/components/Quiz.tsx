86db19e57a4c75e70782f5467185bfccf782acbc
JAEMIN YOON <yjm8440@naver.com>
2023-09-25 01:01:09 +0900

    Feature: 퀴즈 일단 대충 크기만 맞춤

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -22,12 +22,7 @@ const Quiz: React.FC = () => {
         height={869}
       />
       <SideImageContainer>
-        <SideImage
-          src={"/images/graduationCap.png"}
-          alt="graduationCap"
-          width={595}
-          height={595}
-        />
+        <SideImage src={"/images/graduationCap.png"} alt="graduationCap" />
       </SideImageContainer>
       <GrayCircle />
     </QuizWrapper>
@@ -43,12 +38,21 @@ const GrayCircle = styled.div`
   width: 845px;
   height: 845px;
   z-index: 0;
+
+  @media (max-width: 768px) {
+    display: none;
+  }
 `;
 
 const PurpleTitle = styled.div`
   font-weight: 700;
   font-size: 30px;
   color: #813ff2;
+
+  @media (max-width: 768px) {
+    font-size: 16px;
+    margin-top: 140px;
+  }
 `;
 
 const Content = styled.div`
@@ -57,6 +61,11 @@ const Content = styled.div`
   color: #1a1f27;
   margin-top: 6px;
   line-height: 54px;
+
+  @media (max-width: 768px) {
+    font-size: 16px;
+    line-height: 26px;
+  }
 `;
 
 const Title = styled.div`
@@ -64,6 +73,10 @@ const Title = styled.div`
   font-size: 50px;
   color: #1a1f27;
   margin-top: 8px;
+
+  @media (max-width: 768px) {
+    font-size: 32px;
+  }
 `;
 
 const QuizDescription = styled.div`
@@ -71,6 +84,12 @@ const QuizDescription = styled.div`
   flex-direction: column;
   align-items: flex-start;
   margin-bottom: 435px;
+
+  @media (max-width: 768px) {
+    align-items: center;
+    text-align: center;
+    margin-bottom: 0px;
+  }
 `;
 
 const PhoneImage = styled.img`
@@ -78,11 +97,26 @@ const PhoneImage = styled.img`
   z-index: 1;
   margin-left: auto;
   margin-right: auto;
+
+  width: 489px;
+  height: 869px;
+
+  @media (max-width: 768px) {
+    width: 280px;
+    height: 373px;
+  }
 `;
 
 const SideImage = styled.img`
   object-fit: contain;
   z-index: 1;
+  width: 595px;
+  height: 595px;
+
+  @media (max-width: 768px) {
+    width: 218px;
+    height: 218px;
+  }
 `;
 
 const QuizWrapper = styled.div`
@@ -93,6 +127,13 @@ const QuizWrapper = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: flex-end;
+
+  @media (max-width: 768px) {
+    height: 812px;
+    display: inline-flex;
+    flex-direction: column;
+    align-items: center;
+  }
 `;
 
 const SideImageContainer = styled.div`
