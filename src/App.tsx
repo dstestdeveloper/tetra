8cfa66232ca0fcacc2837bfadbd660065867efdf
yoooonjaemin <123533853+yoooonjaemin@users.noreply.github.com>
2023-09-25 19:22:53 +0900

    Feature: Animation (#1)
    * Feature: 화면 시도중
    
    * Feature: 애니메이션 추가 완료
    
    * Feature: 애니메이션 천천히 추가
    
    * Feature: 올라갈때 버그 해결
    
    * Feature: todo 다시 정리

--- src/App.tsx
+++ src/App.tsx
@@ -7,27 +7,147 @@ import Lecture from "./components/Lecture";
 import Quiz from "./components/Quiz";
 import SelfCoaching from "./components/SelfCoaching";
 import Footer from "./components/Footer";
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import "./App.css";
 
 function App() {
   // TODO
   // 모바일 퀴즈, 커뮤니티, 강의, 상담, 배치 다시
-  // pc애니메이션 추가
   // font적용
   // 헤더 따라 내려오도록
+
+  const [activeSection, setActiveSection] = useState(0);
+  const [sectionPositions, setSectionPositions] = useState<number[]>([]);
+
+  useEffect(() => {
+    const sections = document.querySelectorAll<HTMLElement>(".section");
+    const positions = Array.from(sections).map((s) => s.offsetTop);
+    setSectionPositions(positions);
+  }, []);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollYMid = window.scrollY + window.innerHeight / 2;
+      for (let i = 0; i < sectionPositions.length; i++) {
+        if (
+          scrollYMid >= sectionPositions[i] &&
+          (i === sectionPositions.length - 1 ||
+            scrollYMid < sectionPositions[i + 1])
+        ) {
+          setActiveSection(i);
+          return;
+        }
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [sectionPositions]);
 
   return (
     <>
       <Header />
       <Intro />
       <SelfCoaching />
-      <Quiz />
-      <Community />
-      <Lecture />
-      <Consulting />
+
+      <DesktopOnly>
+        <EmptySection className="section">
+          <InnerContent
+            className={`fixed-content ${
+              activeSection === 0 ? "fade-in" : "fade-out"
+            }`}
+          ></InnerContent>
+        </EmptySection>
+        <Section className="section">
+          <InnerContent
+            className={`fixed-content ${
+              activeSection === 1 ? "fade-in" : "fade-out"
+            }`}
+          >
+            {activeSection === 1 ? <Quiz /> : null}
+          </InnerContent>
+        </Section>
+        <Section className="section">
+          <InnerContent
+            className={`fixed-content ${
+              activeSection === 2 ? "fade-in" : "fade-out"
+            }`}
+          >
+            {activeSection === 2 ? <Community /> : null}
+          </InnerContent>
+        </Section>
+        <Section className="section">
+          <InnerContent
+            className={`fixed-content ${
+              activeSection === 3 ? "fade-in" : "fade-out"
+            }`}
+          >
+            {activeSection === 3 ? <Lecture /> : null}
+          </InnerContent>
+        </Section>
+        <Section className="section">
+          <InnerContent
+            className={`fixed-content ${
+              activeSection === 4 ? "fade-in" : "fade-out"
+            }`}
+          >
+            {activeSection === 4 ? <Consulting /> : null}
+          </InnerContent>
+        </Section>
+        <EmptySection className="section"></EmptySection>
+      </DesktopOnly>
+
+      <MobileOnly>
+        <Quiz />
+        <Community />
+        <Lecture />
+        <Consulting />
+      </MobileOnly>
+
       <Outro />
       <Footer />
     </>
   );
 }
+
+const EmptySection = styled.div`
+  height: 30vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+`;
+
+const Section = styled.div`
+  height: 200vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+`;
+
+const InnerContent = styled.div`
+  position: fixed;
+  top: 0;
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  pointer-events: none;
+`;
+
+const DesktopOnly = styled.div`
+  @media (max-width: 768px) {
+    display: none;
+  }
+`;
+
+const MobileOnly = styled.div`
+  @media (min-width: 769px) {
+    display: none;
+  }
+`;
 
 export default App;
